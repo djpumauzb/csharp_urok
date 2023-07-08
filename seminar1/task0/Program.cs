4b5369da4a75e016bfbb51faf34eca07b99f06dd
Sirojiddin <sirojboy@inbox.ru>
2023-07-08 20:01:27 +0500

    add seminar1

---
+++ seminar1/task0/Program.cs
@@ -0,0 +1,6 @@
+﻿// Napishite programmu, kotoraya ne vxod prinimaet celoe chislo i vidaet ego kvadrat (chislo umnojenie samogo sebya)
+
+Console.Write("VVedite chislo dlya kvadrata: ");
+int number = int.Parse(Console.ReadLine());
+int kvadrat = number * number;
+Console.WriteLine($"Kvadrat chisla {number} raven na: {kvadrat}");
