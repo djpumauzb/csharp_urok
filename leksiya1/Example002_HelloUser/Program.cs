e593cc85010d337a35508730a2e40c9a9569f4b5
Sirojiddin <sirojboy@inbox.ru>
2023-07-08 23:28:54 +0500

    peremeshen v papku Leksiya

--- leksiya1/Example002_HelloUser/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Ismingizni yozing ");
-string username = Console.ReadLine();
-Console.WriteLine("Salom, "); 
-Console.WriteLine(username);
-
-// Console.Write("Ismingizni yozing: ");
-// string username = Console.ReadLine();
-
-// Console.WriteLine("Salom, " + (username != null ? username : ""));
