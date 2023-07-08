5e97d2a84e8a82a10c540f9fcb09b2c0475a9d10
Sirojiddin <sirojboy@inbox.ru>
2023-07-08 22:31:30 +0500

    done task5

--- seminar1/task5/Program.cs
+++ seminar1/task5/Program.cs
@@ -1,4 +1,11 @@
 ﻿// Napishite programmu, kotoraya na vxod prinimaet odno chislo (N)
 // a na vixode pokazivaet vse celie chisla v promejutke ot -N do N
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
 
-Console.WriteLine("Hello, World!");
+Console.Write("Vvedite chislo: ");
+int N = int.Parse(Console.ReadLine());
+int minusN = N * -1;
+for (int i = minusN; i <= N; i++) {
+    Console.Write($"{i} ");
+}
