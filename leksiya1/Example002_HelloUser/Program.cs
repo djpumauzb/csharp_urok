5e04415f354e0c403235c0bcaaad1ba086d87584
Sirojiddin <sirojboy@inbox.ru>
2023-07-08 20:01:13 +0500

    add leksiya1

---
+++ leksiya1/Example002_HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Ismingizni yozing ");
+string username = Console.ReadLine();
+Console.WriteLine("Salom, "); 
+Console.WriteLine(username);
+
+// Console.Write("Ismingizni yozing: ");
+// string username = Console.ReadLine();
+
+// Console.WriteLine("Salom, " + (username != null ? username : ""));
---
+++ leksiya1/Example004_Div/Program.cs
@@ -0,0 +1,16 @@
+﻿int numberA = 12;
+int numberB = 5;
+Console.WriteLine(numberA / numberB);
+
+// ispolzuem doble vmesto int
+double numberC = 12;
+double numberD = 5;
+Console.WriteLine(numberC / numberD);
+
+// With random
+int numberX = new Random().Next(1, 10);
+Console.WriteLine(numberX);
+int numberY = new Random().Next(1, 10);
+Console.WriteLine(numberY);
+int result = numberX + numberY;
+Console.WriteLine(result);
