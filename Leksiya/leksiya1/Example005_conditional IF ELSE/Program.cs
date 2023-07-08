c0f3be3486a3e08729b93172654133c1727870bf
Sirojiddin <sirojboy@inbox.ru>
2023-07-08 23:26:47 +0500

    dobavlen v papku leksiya

---
+++ Leksiya/leksiya1/Example005_conditional IF ELSE/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
