361bd7a1d4fd03adb588ef9ac15246baa34ec553
Sirojiddin <sirojboy@inbox.ru>
2023-07-08 23:27:37 +0500

    dobavlen v papku Smeninar

---
+++ Seminar/seminar1/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Napishite programmu, kotoraya na vxod prinimaet dva chisla i
+// proveryaet yavlaetsya li pervoe chislo kvadratom vtorogo
+// a = 25, b = 5 -> da 
+// a = 2, b = 10 -> net
+// a = 9, b = -3 -> da
+// a = -3, b = 9 -> net
+
+Console.Write("Vvedite chislo: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Vvedite kvadrat etogo chisla: ");
+int number2 = int.Parse(Console.ReadLine());
+int kvadrat = number1 * number1;
+if (number2 == kvadrat) {
+    Console.WriteLine("da");
+} else {
+    Console.WriteLine("net");
+}
+
