af2f9b5b06cb89b0f6eccc264acef50b9ba880f7
Sirojiddin <sirojboy@inbox.ru>
2023-07-08 22:16:46 +0500

    add task 7

---
+++ seminar1/task7/Program.cs
@@ -0,0 +1,8 @@
+﻿// Napishite programmi, kotoraya prinimaet 
+// na vxod trexznachnoe chislo i na vixode pokazivaet poslednuyu cifru etogo chisla
+// 456 -> 6
+// 782 -> 2
+
+
+
+Console.WriteLine("Hello, World!");
