411ccc11f61f78439bbf47e894e8416b08665022
Sirojiddin <sirojboy@inbox.ru>
2023-07-08 21:20:30 +0500

    done task3

--- seminar1/task3/Program.cs
+++ seminar1/task3/Program.cs
@@ -3,4 +3,23 @@
 // 5-> Pyatnica
 
 
-Console.WriteLine("Hello, World!");
+Console.Write("Vvedite chislo dnya nedeli: ");
+int number = int.Parse(Console.ReadLine());
+if (number == 1) {
+    Console.WriteLine("Dushanba");
+} else if (number == 2) {
+    Console.WriteLine("Seshanba");
+} else if (number == 3) {
+    Console.WriteLine("Chorshanba");
+} else if (number == 4) {
+    Console.WriteLine("Payshanba");
+} else if (number == 5) {
+    Console.WriteLine("Juma");
+} else if (number == 6) {
+    Console.WriteLine("Shanba");
+} else if (number == 7) {
+    Console.WriteLine("Yakshanba");
+} else {
+    Console.WriteLine("V nedeli sushastvuet tolko 7 dney.");
+    Console.WriteLine("Vvedite znachenie 1 do 7");
+}
