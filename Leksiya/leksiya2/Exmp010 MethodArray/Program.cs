2de32f0a3093ad35448c867767641982907e1616
Sirojiddin <sirojboy@inbox.ru>
2023-07-09 20:18:00 +0500

    add exmp 010

---
+++ Leksiya/leksiya2/Exmp010 MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {108, 209, 153, 405, 1015, 46, 870, 180, 296};
+
+int n = array.Length;
+int find = 153;
+int index = 0;
+
+while (index < n) {
+    if (array[index] == find) {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
