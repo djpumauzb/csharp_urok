31ddba4247a0268b54f81da7c2a144764718bb20
Sirojiddin <sirojboy@inbox.ru>
2023-07-10 23:17:12 +0500

    done hw4

--- DZ/Seminar1/hw4/Program.cs
+++ DZ/Seminar1/hw4/Program.cs
@@ -3,4 +3,24 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Hello, World!");
+Console.Write("Napishite 1oe chislo: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Napishite 2oe chislo: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Napishite 3oe chislo: ");
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 == num2 & num2 == num3) {
+    Console.WriteLine("oni vse odinakovie )");
+}
+
+
+int max = 0;
+
+if (max < num1) max = num1;
+if (num1 < num2) max = num2;
+if (num2 < num3) max = num3;
+
+if (num1 != num2 & num2 != num3) {
+Console.WriteLine($"Max = {max}");
+}
