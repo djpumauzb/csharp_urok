7bb4725eb5b98854718398dea96277c309023ba0
Sirojiddin <sirojboy@inbox.ru>
2023-07-10 22:38:32 +0500

    done dz2

--- DZ/Seminar1/hw2/Program.cs
+++ DZ/Seminar1/hw2/Program.cs
@@ -3,5 +3,19 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.Write("Napishite 1oe chislo: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Napishite 2oe chislo: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 == num2) {
+    Console.WriteLine("Vvedite razni chisla");
+    Console.WriteLine("odni odinakovie )");     // dlya togo shtobi polzovatel ne vvel odinakovie znachenie )
+}
 
-Console.WriteLine("Hello, World!");
+int max = 0;
+if (num1 > num2) max = num1;
+if (num1 < num2) max = num2;
+
+if (num1 == max || num2 == max) {               // a eto chto bi ne srabativalo etot writeline sam po sebe, atolko togda kogda doydet do syuda) 
+Console.WriteLine($"Max = {max}");
+}
