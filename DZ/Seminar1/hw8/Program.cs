e23fc1463f21a6d45199c96aef7f124b0fcc3f67
Sirojiddin <sirojboy@inbox.ru>
2023-07-10 23:47:30 +0500

    add hw8 half solve

--- DZ/Seminar1/hw8/Program.cs
+++ DZ/Seminar1/hw8/Program.cs
@@ -2,4 +2,20 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.WriteLine("Hello, World!");
+Console.Write("Vvedite chislo: ");
+int N = int.Parse(Console.ReadLine());
+int i = 0;
+if (N == 0 || N < 0) Console.WriteLine("Vvedite chislo bolshe 0 !!!");
+
+while (i < N & N != 0) {
+    if (i % 2 == 0 & i != 0) {
+        Console.Write($"{i}, ");
+    }
+    i++;
+}
+
+if (N != 0 || N > 0) {
+    if (N % 2 == 0) {
+    Console.WriteLine(N);
+    }
+}
