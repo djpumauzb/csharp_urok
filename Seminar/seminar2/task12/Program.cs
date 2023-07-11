1bb555dd2a48c88ebe1501451c4e498ad49ef505
Sirojiddin <sirojboy@inbox.ru>
2023-07-11 06:30:22 +0500

    add task12

---
+++ Seminar/seminar2/task12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Napishite programmu, kotoraya budet prinimat na vxod 2 chisla i vivodit, yavlyaetsya
+// li 2oe chislo kratnim pervomu. Esli chislo 2 ne kratno chislu 1, to
+// programma vivodit ostatok ot deleniya
+
+// 34, 5 -> ne kratno, ostatok 4
+// 16, 4 -> kratno
+
+Console.Write("Vvedite 1oe chislo: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Vvedite 2oe chislo: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int ostatok = num1 % num2; // ludshe s peremnnoi rabotat )
+
+if (ostatok == 0) {
+    Console.WriteLine("Kratno");
+}
+if (ostatok != 0) {
+    Console.WriteLine(ostatok);
+}
