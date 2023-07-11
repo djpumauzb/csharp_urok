b53649a998915c7a237117fd6ab4da7ec841f63d
Sirojiddin <sirojboy@inbox.ru>
2023-07-11 22:47:54 +0500

    done hw 15

--- DZ/Seminar2/hw15/Program.cs
+++ DZ/Seminar2/hw15/Program.cs
@@ -1,4 +1,29 @@
-﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 1 || num > 7) {
+    Console.WriteLine("Vvedite chislo 1 do 7");
+} else {
+    int monday = 1;
+    int tuesday = 2;
+    int wednesday = 3;
+    int thursday = 4;
+    int friday = 5;
+    int saturday = 6;
+    int sunday = 7;
+    if (num == saturday || num == sunday) {
+        Console.WriteLine($"{num} -> yes");
+    } else {
+        Console.WriteLine($"{num} -> no");
+    }
+
+}
+
+
