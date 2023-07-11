9df065b3007be9bdcd74bf53ebc83c8705398ea2
Sirojiddin <sirojboy@inbox.ru>
2023-07-11 05:53:55 +0500

    add tsk0 sem2

---
+++ Seminar/seminar2/task0/Program.cs
@@ -0,0 +1,24 @@
+﻿// Napishite funksiyu, kotoraya vivodit sluch chislo iz trezka [10, 99] 
+// i pokazivaet naibolshuyu cifru chisla
+
+// Exmp:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+// generirovat sluch chisl
+// zapisat cifru po otdelnosti
+// sravnit i vivesti naiboluyu
+
+int num = new Random().Next(10, 100);
+int dig1 = num / 10;  // vozmet 1oe chislo
+int dig2 = num % 10;  // vozmet ostatok (2oe) chislo
+
+int max = dig1;
+
+if (max < dig2) 
+{
+     max = dig2;
+}
+
+Console.WriteLine($"Max cifra v chisle {num} -> {max}");
