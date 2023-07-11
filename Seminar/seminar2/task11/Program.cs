33471c58bde866fd60dee66a4377bad2494079d0
Sirojiddin <sirojboy@inbox.ru>
2023-07-11 06:03:09 +0500

    add task11 and done

---
+++ Seminar/seminar2/task11/Program.cs
@@ -0,0 +1,18 @@
+﻿// Napishite programmu, kotoraya vivodit sluchaynoe texznachnoe chislo
+// i udalyaet vtoruyu  cifru etogo chisla
+
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+
+
+
+//int num = new Random().Next(100, 1000);
+
+int num = 798;
+int dig1 = num / 100;  
+int dig2 = num % 10;  
+
+Console.WriteLine($"{dig1}{dig2}");
