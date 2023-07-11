e273ef19e6e91e93e26ba1e2284cf7fb9bec4961
Sirojiddin <sirojboy@inbox.ru>
2023-07-11 06:46:00 +0500

    add tsk14

---
+++ Seminar/seminar2/task14/Program.cs
@@ -0,0 +1,18 @@
+﻿// Napsihite programmu, kotoraya prinimaet na vxod chislo i proveryaet,
+// kratno li ono odnovremenno 7 i 23
+
+// 14 - > net
+// 46 -> net
+// 161 -> da
+
+Console.Write("Vvedite chislo: ");
+int num = int.Parse(Console.ReadLine());
+
+int check1 = num % 7;
+int check2 = num % 23;
+
+if (check1 == 0 & check2 == 0) {
+    Console.WriteLine("Da");
+} else {
+    Console.WriteLine("Net");
+}
