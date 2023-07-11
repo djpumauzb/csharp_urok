3dfaf8fe62fc7fed182e0cc488d1d11fe3a7a1d6
Sirojiddin <sirojboy@inbox.ru>
2023-07-11 22:17:16 +0500

    add hw13 not vicely done :

--- DZ/Seminar2/hw13/Program.cs
+++ DZ/Seminar2/hw13/Program.cs
@@ -1,4 +1,27 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.Write("Vvedite chislo: ");
+// double num = double.Parse(Console.ReadLine());
+
+// if (num < 100) {
+//     Console.WriteLine($"{num} -> третьей цифры нет");
+// } else {
+//     double result = (num / 100) % 10;
+//     Console.WriteLine($"{num} -> {result}");
+// }
+
+
+
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+
+if (num.Length < 3) {
+    Console.WriteLine($"{num} -> третьей цифры нет");
+} else {
+    char result = num[2];
+    Console.WriteLine($"{num} -> {result}");
+}
