2b741d32b1da76cedb994b1f31df27867598e931
Sirojiddin <sirojboy@inbox.ru>
2023-07-14 16:55:12 +0500

    add tsk1 replacer

--- Leksiya/leksiya3/tsk1/Program.cs
+++ Leksiya/leksiya3/tsk1/Program.cs
@@ -9,3 +9,31 @@ string text = "- Ya dumayu. - skazal knyaz', ulibayac', - chto,"
             + "ejeli bi vas poslali vmesto nashego milogo Vincengrerode,"
             + "vi bi vzyali pritupom soglasie prusskogo korolya."
             + "Vi tak krasnorechivi. Bi dadite mne chayu?";
+
+// string s = "qwerty"
+//             0123
+// s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'k', 'K');
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'S', 's');
+Console.WriteLine(newText);
+Console.WriteLine();
