d2295f6cc9ae5e93978628fb0c04854847934f55
Sirojiddin <sirojboy@inbox.ru>
2023-07-14 17:07:12 +0500

    add tsk2

---
+++ Leksiya/leksiya3/tsk2/Program.cs
@@ -0,0 +1,37 @@
+﻿// tsk2
+// poryadka po ocheredi v massive
+
+
+int[] arr = {1, 5, 4, 1, 6, 7, 1, 8};
+
+void PrntArr(int[] arr)
+{
+    int count = arr.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectSort(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        int minPos = i;
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if(arr[j] < arr[minPos]) minPos = j;
+        }
+        int temporary = arr[i];
+        arr[i] = arr[minPos];
+        arr[minPos] = temporary;
+    }
+}
+
+
+PrntArr(arr);
+SelectSort(arr);
+
+PrntArr(arr);
