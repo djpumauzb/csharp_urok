2c0bedef3c33d30f6ad06239a290667dfc009d7f
Sirojiddin <sirojboy@inbox.ru>
2023-07-14 10:31:22 +0500

    done hw19

--- DZ/Seminar3/hw19/Program.cs
+++ DZ/Seminar3/hw19/Program.cs
@@ -3,7 +3,18 @@
 // 12821 -> да
 // 23432 -> да
 
-
+string IsPalindrome(int num)
+{
+    if (num >= 99999 || num < 10000) return "Введите только пятизначное число !";
+    string strnum = num.ToString();
+    int lenght = strnum.Length;
+    string input = $"{num} -> ";
+    for (int i = 0; i < lenght / 2; i++)
+    {
+        if (strnum[i] != strnum[lenght - i - 1]) return input + "no";
+    }
+    return input + "yes";
+}
 
 int GetInfo(string msg)
 {
@@ -12,4 +23,5 @@ int GetInfo(string msg)
     return inputNum;
 }
 
-int num = GetInfo("Vvedite chisl: ");
+int num = GetInfo("Vvedite chislo: ");
+Console.WriteLine(IsPalindrome(num));
