f1fbc1fc09457062bd67d0f0ea919376002cff10
Sirojiddin <sirojboy@inbox.ru>
2023-07-14 14:08:25 +0500

    add leksiya3

---
+++ Leksiya/leksiya3/tsk1/Program.cs
@@ -0,0 +1,11 @@
+﻿// === Rabota s tekstom
+// Dan tekst. V tekste nujno vse problemi zamenit cehrtochkami,
+// malenkie bukvi "k" zamenit na bolshimi "K"
+// a bolshie "C" zamenit na maleknkim "c"
+
+// Yasno li zadacha ?)
+
+string text = "- Ya dumayu. - skazal knyaz', ulibayac', - chto,"
+            + "ejeli bi vas poslali vmesto nashego milogo Vincengrerode,"
+            + "vi bi vzyali pritupom soglasie prusskogo korolya."
+            + "Vi tak krasnorechivi. Bi dadite mne chayu?";
