ff153f0b116315c5707928c5fb0618919077e138
Sirojiddin <sirojboy@inbox.ru>
2023-07-17 11:53:00 +0500

    done hw 27 bruh

--- DZ/Seminar4/hw27/Program.cs
+++ DZ/Seminar4/hw27/Program.cs
@@ -2,3 +2,28 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int GetSumm(int number)
+{
+    string numStr = number.ToString();
+    int sum = 0;
+    for (int i = 0; i < numStr.Length; i++)
+    {
+        sum = sum + int.Parse(numStr[i].ToString());
+    }
+    
+    return sum;
+}
+
+
+
+
+int GetInfo(string msg)
+{
+    Console.Write(msg);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int num = GetInfo("Vvedite cislo: ");
+Console.WriteLine($"{num} -> {GetSumm(num)}");
