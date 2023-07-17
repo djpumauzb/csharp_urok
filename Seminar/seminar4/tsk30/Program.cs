4630d822597b513e50ff50e63ea9782e13394beb
Sirojiddin <sirojboy@inbox.ru>
2023-07-17 07:04:26 +0500

    add tsk 30

---
+++ Seminar/seminar4/tsk30/Program.cs
@@ -0,0 +1,16 @@
+﻿// Napishite programmu, kotoraya vivodit massiv iz 8 elementov, 
+// zapolnenniy nulyami i edinicami v sluchaynom poryadke
+// [1,0,1,1,1,0]
+
+
+int[] massiv = new int[8];
+Random randomNum = new Random();
+for (int i = 0; i < 8; i++)
+{
+    massiv[i] = randomNum.Next(0, 2);
+}
+
+string joinedArray = string.Join(",", massiv);
+Console.WriteLine(joinedArray);
+
+
