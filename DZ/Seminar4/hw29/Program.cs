28f2a657fe47d61652bbd160dc65a14cda1d8110
Sirojiddin <sirojboy@inbox.ru>
2023-07-17 16:20:11 +0500

    done hw29 bruuuuuuuuuuuuh with GPT :(

--- DZ/Seminar4/hw29/Program.cs
+++ DZ/Seminar4/hw29/Program.cs
@@ -1,3 +1,31 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+
+int[] AddArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = GetInfo($"Введите элемент {i + 1}: ");
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine("Массив: [" + string.Join(", ", array) + "]");
+}
+
+int GetInfo(string msg)
+{
+    Console.Write(msg);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+
+int size = 8;
+int[] nums = AddArray(size);
+PrintArray(nums);
