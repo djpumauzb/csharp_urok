b142e18b9dc239b68e6af58d3383c86006318197
Sirojiddin <sirojboy@inbox.ru>
2023-07-19 09:52:41 +0500

    done hw 34

--- DZ/Seminar5/hw34/Program.cs
+++ DZ/Seminar5/hw34/Program.cs
@@ -2,3 +2,29 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+
+
+int GetEvenNum (int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int[] NewRandomArray(int size, int minValue, int maxValue) // Pechataet randomnie massivi s opredelonnimi diapazonami
+{
+    int[] massiv = new int[size];
+    for (int i = 0; i < size; i++)
+        massiv[i] = new Random().Next(minValue, maxValue);
+    return massiv;
+}
+int[] array = NewRandomArray(4, 100, 1000);
+
+Console.Write($"[{string.Join(",", array)}] -> {GetEvenNum(array)}");
