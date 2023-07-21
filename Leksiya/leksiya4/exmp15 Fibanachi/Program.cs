e4e763b2739d00e1b9f2a18dbc788e661d94257b
Sirojiddin <sirojboy@inbox.ru>
2023-07-21 17:32:34 +0500

    add exmp15

---
+++ Leksiya/leksiya4/exmp15 Fibanachi/Program.cs
@@ -0,0 +1,17 @@
+﻿// Fibanachi
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n-1) + f(n-2)
+
+double Fibanachi(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibanachi(n-1) + Fibanachi(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibanachi(i)}");
+}
+
+// Interesno fibacnachi s f(38) nachinaet medlenno rashiataet po dajdoy dobavlennoy stoke nachianetsya eshe medleneee 🤔
