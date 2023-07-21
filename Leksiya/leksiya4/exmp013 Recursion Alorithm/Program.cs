e79986f037a10fc529abeded280973b42fb7bb00
Sirojiddin <sirojboy@inbox.ru>
2023-07-21 16:27:45 +0500

    add leks4

---
+++ Leksiya/leksiya4/exmp013 Recursion Alorithm/Program.cs
@@ -0,0 +1,46 @@
+﻿
+string[,] table = new string[2, 5];
+// Stroki po umolchaniyu inicializirovanniy inicializovannoy strokoy String.Empty
+// snachala stroki potom stolbci // Birinchi qatorlar keladi keyin esa ustinlar
+// table[0,0] table[0,1] table[0,2] ... table[0,5]
+// table[1,0]
+// table[2,0]
+
+// table[1, 2]="someting";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10); // [1; 10]
+        }    
+    }
+}
+
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
