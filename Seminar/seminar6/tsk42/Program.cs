ebf1e1db3814914ab69a7a6e3dca94421a2c2905
Sirojiddin <sirojboy@inbox.ru>
2023-07-23 16:07:53 +0500

    add tsk42

---
+++ Seminar/seminar6/tsk42/Program.cs
@@ -0,0 +1,27 @@
+﻿// Napishite programmu kotoraya budet pereobrazovivat'
+// destichnoe chislo v dvoichnoie.
+
+// 45 -> 101101
+// 3 -> 11
+// 2-> 10
+
+void GetBinayrNum(int num)
+{
+    string temp = string.Empty;
+    while (num > 0)
+    {
+        temp = (num % 2).ToString() + temp; // berete s konca poetomu + temp
+        num = num / 2;
+    }
+    Console.Write(temp);
+}
+
+int GetInfo(string msg)
+{
+    Console.Write(msg);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int num = GetInfo("Vvedite chislo: ");
+GetBinayrNum(num);
