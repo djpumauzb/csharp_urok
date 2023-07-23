8a1bf47eefcb4013024d0d16f7cc5ec07e5fad27
Sirojiddin <sirojboy@inbox.ru>
2023-07-23 17:09:03 +0500

    add nwer way

---
+++ Seminar/seminar6/tsk45/Program.cs
@@ -0,0 +1,29 @@
+﻿// Napishite programmu, kotoraya budet sozdavat' 
+//kopiyu massiva s pomoshyu poelementnogo kopirovaniya
+
+void ReArray(int[] array) 
+{
+    int copy = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+       copy = array[i];
+       Console.Write(copy);
+    }
+}
+
+
+
+
+
+
+int[] NewRandomArray(int size, int minValue, int maxValue)
+{
+    int[] massiv = new int[size];
+    for (int i = 0; i < size; i++)
+        massiv[i] = new Random().Next(minValue, maxValue);
+    return massiv;
+}
+
+int[] array = NewRandomArray(5, 1, 10);
+int[] reArray = ReArray(array);
+Console.Write($"[{string.Join(",", array)}] -> {reArray} ");
