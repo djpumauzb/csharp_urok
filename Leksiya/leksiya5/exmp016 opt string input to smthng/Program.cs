84015e12823896e120a50a9865431079b4aeb56a
Sirojiddin <sirojboy@inbox.ru>
2023-07-27 11:05:50 +0500

    add leks 5

---
+++ Leksiya/leksiya5/exmp016 opt string input to smthng/Program.cs
@@ -0,0 +1,25 @@
+﻿// string text = "(1,2) (2,3) (4,5) (6,7)";
+
+using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+                                .Replace("(", "")
+                                .Replace(")", "")
+                                ;
+
+Console.WriteLine(text);
+
+var data = text.Split(" ")
+                .Select(item => item.Split(","))
+                .Select(cordinate => (int.Parse(cordinate[0], int.Parse(cordinate[1]))))
+                .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    // Console.WriteLine(data[i]);
+    for (int j = 0; j < data[i].Length; j++)
+    {
+        Console.WriteLine(data[i][j]);
+    }
+    Console.WriteLine();
+}
