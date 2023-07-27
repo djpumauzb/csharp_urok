37d9361a0a2b1361917c7809095ec692b8c315fe
Sirojiddin <sirojboy@inbox.ru>
2023-07-27 11:15:40 +0500

    the newer method2

--- Leksiya/leksiya5/exmp016 opt string input to smthng/Program.cs
+++ Leksiya/leksiya5/exmp016 opt string input to smthng/Program.cs
@@ -13,7 +13,7 @@ var data = text.Split(" ")
                .Select(item => item.Split(","))
                .Select(cordinate => (x: int.Parse(cordinate[0]), y: int.Parse(cordinate[1])))
                .Where(e => e.x % 2 == 0)
-               .Select(point => (point.x * 10, point.y))
+               .Select(point => (point.x * 10, point.y + 10))
                .ToArray();
 
 for (int i = 0; i < data.Length; i++)
