d8e2d296d350bd735a3e3efd0a17fbe462c495ab
Sirojiddin <sirojboy@inbox.ru>
2023-07-27 11:14:28 +0500

    the newer method

--- Leksiya/leksiya5/exmp016 opt string input to smthng/Program.cs
+++ Leksiya/leksiya5/exmp016 opt string input to smthng/Program.cs
@@ -10,12 +10,14 @@ string text = "(1,2) (2,3) (4,5) (6,7)"
 Console.WriteLine(text);
 
 var data = text.Split(" ")
-                .Select(item => item.Split(","))
-                .Select(cordinate => (x: int.Parse(cordinate[0]), y: int.Parse(cordinate[1])))
-                .ToArray();
+               .Select(item => item.Split(","))
+               .Select(cordinate => (x: int.Parse(cordinate[0]), y: int.Parse(cordinate[1])))
+               .Where(e => e.x % 2 == 0)
+               .Select(point => (point.x * 10, point.y))
+               .ToArray();
 
 for (int i = 0; i < data.Length; i++)
 {
-    Console.WriteLine(data[i].x*10);
+    Console.WriteLine(data[i]);
     Console.WriteLine();
 }
