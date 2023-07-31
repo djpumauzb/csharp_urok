96a55010a6b524ada2b4cf1cdfaa57f2ba0a6d7c
Sirojiddin <sirojboy@inbox.ru>
2023-07-31 12:35:57 +0500

    add tsk 59

---
+++ Seminar/seminar8/tsk59/Program.cs
@@ -0,0 +1,14 @@
+﻿// Zadayte dvumerniy massiv iz celix chisel.
+// Napishite programmu, kotoraya udalit stroku i stolbec, 
+// na peresechenii kotorix raspolojen naimenshiy element massiva
+// naprimer zadan massiv:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Naimenshiy element - 1, na vixode poluchim sleduyushiy massiv:
+// 9 4 2
+// 2 2 6
+// 3 4 7 
+
