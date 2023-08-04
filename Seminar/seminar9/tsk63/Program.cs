7c8cc82e17790b4aa8ff8d0804e7a6d362086c67
Sirojiddin <sirojboy@inbox.ru>
2023-08-04 22:32:43 +0500

    add tsk 63 & 65 done

---
+++ Seminar/seminar9/tsk63/Program.cs
@@ -0,0 +1,29 @@
+﻿// Zadayte znachnie N. Napishite programmu, kotoraya viviedet vse 
+// naturalnie chisla v promejutka ot 1 do N.
+// N = 5 -> "1,2,3,4,5"
+// N = 6 -> "1,2,3,4,5,6"
+
+void NaturalNumber(int N)
+{
+    Console.Write($"{N} -> ");
+    for (int i = 1; i <= N; i++)
+    {
+        Console.Write($"{i}");
+        if (i < N)
+        {
+            Console.Write(",");
+        }
+    }
+}
+
+
+int GetInfo(string msg)
+{
+    Console.Write(msg);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int N = GetInfo("Vvedite znachenie N: ");
+NaturalNumber(N);
+
---
+++ Seminar/seminar9/tsk65/Program.cs
@@ -0,0 +1,32 @@
+﻿// Zadayte znachenie M i N. Napishite programmu, 
+// kotoraya vivedet vse naturalnie chisla v promejutke M do N.
+// M = 1; N = 5 -> "1,2,3,4,5"
+// M = 4; N = 8 -> "4,6,7,8"
+
+
+
+void NaturalNumber(int M, int N)
+{
+    Console.Write($"M = {M}; N = {N} -> ");
+    for (int i = M; i <= N; i++)
+    {
+        Console.Write($"{i}");
+        if (i < N)
+        {
+            Console.Write(",");
+        }
+    }
+}
+
+
+int GetInfo(string msg)
+{
+    Console.Write(msg);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int M = GetInfo("Vvedite znachenie M: ");
+int N = GetInfo("Vvedite znachenie N: ");
+
+NaturalNumber(M, N);
