2c9e486cefa8dee95551d6b2ac7cd6ce5f3057a8
Sirojiddin <sirojboy@inbox.ru>
2023-08-05 19:33:43 +0500

    done hw 64

--- DZ/Seminar9/hw64/Program.cs
+++ DZ/Seminar9/hw64/Program.cs
@@ -3,3 +3,23 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+string NaturalNumber(int N)
+{
+    if (N > 1)
+    {
+        return $"{N}, {NaturalNumber(N - 1)}";
+    }
+    else return Convert.ToString(1);
+}
+
+int GetInfo(string msg)
+{
+    Console.Write(msg);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int N = GetInfo("Vvedite znachenie N: ");
+
+string result = NaturalNumber(N);
+Console.WriteLine($"N = {N} -> {result} ");
