d06af44a0a19235e589dea7a43dfb5960ecc9362
Sirojiddin <sirojboy@inbox.ru>
2023-08-05 22:54:54 +0500

    donw hw68 with 100% GPT IQ:0 :(((

--- DZ/Seminar9/hw68/Program.cs
+++ DZ/Seminar9/hw68/Program.cs
@@ -2,3 +2,32 @@
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+int Acker(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0)
+    {
+        return Acker(m - 1, 1);
+    }
+    else
+    {
+        return Acker(m - 1, Acker(m, n - 1));
+    }
+}
+
+int GetInfo(string msg)
+{
+    Console.Write(msg);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int m = GetInfo("Vvedite znachenie m: ");
+int n = GetInfo("Vvedite znachenie n: ");
+
+int result = Acker(m, n);
+Console.WriteLine($"A({m}, {n}) = {result}");
