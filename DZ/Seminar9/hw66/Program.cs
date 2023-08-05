67e25b21a8400737cd4666898464178f22e792dc
Sirojiddin <sirojboy@inbox.ru>
2023-08-05 22:50:59 +0500

    done hw66

--- DZ/Seminar9/hw66/Program.cs
+++ DZ/Seminar9/hw66/Program.cs
@@ -2,3 +2,30 @@
 // сумму натуральных элементов в промежутке от M до N.
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
+
+int NaturalNumber(int M, int N)
+{
+    if (M > N) // Базовый случай, если M больше N, то возвращаем 0.
+    {
+        return 0;
+    }
+    else
+    {
+        int result = M + NaturalNumber(M + 1, N); // Рекурсивный вызов с увеличением M на 1.
+        return result;
+    }
+}
+
+
+int GetInfo(string msg)
+{
+    Console.Write(msg);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int M = GetInfo("Vvedite znachenie M: ");
+int N = GetInfo("Vvedite znachenie N: ");
+
+int result = NaturalNumber(M, N);
+Console.WriteLine($"M = {M}; N = {N} -> {result} ");
